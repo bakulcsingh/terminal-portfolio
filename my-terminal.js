c5f377a91b57110c40cc568dab0969c4c8dd13d9
Bakul <bakulcsingh@gmail.com>
2024-06-06 15:04:38 -0700

    name

--- my-terminal.js
+++ my-terminal.js
@@ -4,17 +4,28 @@ const term = $("body").terminal(commands, {
   greetings: false,
 });
 
+function trim(str) {
+  return str.replace(/[\n\s]+$/, "");
+}
+
 function render(text) {
   const cols = term.cols();
-  return figlet.textSync(text, {
-    font: font,
-    width: cols,
-    whitespaceBreak: true,
-  });
+  return trim(
+    figlet.textSync(text, {
+      font: font,
+      width: cols,
+      whitespaceBreak: true,
+    })
+  );
 }
 
 function ready() {
-  term.echo(render("Fingers Crossed"));
+  term
+    .echo(() => {
+      const ascii = render("Bakulesh Singh");
+      return `${ascii}\nWelcome to my Portfolio!`;
+    })
+    .resume();
 }
 const font = "Slant";
 
